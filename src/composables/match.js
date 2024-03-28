942e97376d66957a0ce6fc12b833b8c10aa494da
aubry224 <raphael.aubry@etu.unicaen.fr>
2024-03-28 18:05:45 +0100

    Feat: Added ranking view

---
+++ src/composables/match.js
@@ -0,0 +1,33 @@
+import useSupabase from './supabase'
+
+const {supabase} = useSupabase()
+
+export default function useMatch() {
+
+    async function getOrderedRankings() {
+        const { data } = await supabase.from('matchs').select('team1 (name), team2 (name), team1_score, team2_score')
+        let teams = {};
+        for (const match of data) {
+            let t1_name = match.team1.name ? match.team1.name : "Sans nom";
+            let t2_name = match.team2.name ? match.team2.name : "Sans nom";
+            let t1 = match.team1_score
+            let t2 = match.team2_score
+            let t1_result, t2_result;
+            if (t1 > t2) {
+                t1_result = 3;
+                t2_result = 0;
+            } else if (t1 == t2) {
+                t1_result = 1;
+                t2_result = 1;
+            } else {
+                t1_result = 0;
+                t2_result = 3;
+            }
+            teams[t1_name] = t2_result + (teams[t1_name] ? teams[t1_name] : 0)
+            teams[t2_name] = t1_result + (teams[t2_name] ? teams[t1_name] : 0)
+        }
+        return Object.keys(teams).map((key) => [key, teams[key]]).sort((a, b) => b[1] - a[1]);
+    }
+
+    return { getOrderedRankings };
+}
--- src/views/Rankings.vue
+++ src/views/Rankings.vue
@@ -1,15 +1,41 @@
 <script setup>
     import Menu from "../components/Menu.vue";
+    import useMatch from "@/composables/match";
+    import { ref } from "vue";
+
+    const { getOrderedRankings } = useMatch();
+
+    const rankings = ref([])
+
+    getOrderedRankings().then(results => rankings.value = results)
 </script>
 
 <template>
     <Menu></Menu>
-    <section class="flex flex-col items-center gap-4 p-20 bg-[#206090] h-[90vh] ph-[10vh]">
+    <section class="flex flex-col items-center gap-4 p-10 bg-[#206090] h-[90vh] ph-[10vh]">
+        <div class="w-fit flex flex-col gap-4">
+            <div class="text-white team rounded-lg [&:nth-child(1)]:bg-yellow-600 [&:nth-child(2)]:bg-gray-500 [&:nth-child(3)]:bg-red-600 bg-[#185070]" v-for="(team, index) in rankings">
+                <span class="bg-[#183048] rounded-s-lg">{{ index+1 }}</span>
+                <span>{{ team[0] }}</span>
+                <span>{{ team[1] }}</span>
+            </div>
+        </div>
     </section>
 </template>
 
 <style>
     h1, h2, p {
         color: white;
+    }
+
+    .team {
+        display: grid;
+        grid-template-columns: 1fr 5fr 1fr;
+        text-align: center;
+        border: 1px solid white;
+    }
+
+    .team span {
+        padding: 10px;
     }
 </style>
